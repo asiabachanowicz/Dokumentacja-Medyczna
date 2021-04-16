bebddd6897d174f92cf3b2e2438006fc6ffd9cea
Lucek <julianna.wichowska@gmail.com>
2021-04-16 18:13:39 +0200

    Add : database files

--- mysite/login/models.py
+++ mysite/login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Pacjent(models.Model):
+    pacjentID = models.CharField(max_length=30)
+    haslo = models.CharField(max_length=30)
+    login = models.CharField(max_length=30)
+    imie = models.CharField(max_length=30)
+    nazwisko = models.CharField(max_length=30)
+    adres = models.TextField(max_length=30)
+    rodzaj_choroby = models.CharField(max_length=30)
+    nazwa_badania = models.CharField(max_length=30)
+    plec = models.CharField(max_length=30)
+    data_ur = models.DateField(max_length=30)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.db',
     }
 }
 
